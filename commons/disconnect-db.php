a19eba952d7a3895939303b454ce5a8ff7bed365
luongtd15 <luongnek121@gmail.com>
2024-11-07 13:11:43 +0700

    add commons

---
+++ commons/disconnect-db.php
@@ -0,0 +1,3 @@
+<?php
+//ngat ket noi csdl
+$conn = null;
---
+++ commons/env.example.php
@@ -0,0 +1,15 @@
+<?php
+// khai bao cac bien moi truong dung global
+define('PATH_CONTROLLER', __DIR__ . '/../controllers/');
+define('PATH_MODEL', __DIR__ . '/../models/');
+define('PATH_VIEW', __DIR__ . '/../views/');
+
+define('BASE_URL', 'http://localhost/mvc-basic/');
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'sampleProject');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+
+
