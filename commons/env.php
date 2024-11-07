50c8c3e8b17b05df5981d66015e3e0bf7d4e54e4
luongtd15 <luongnek121@gmail.com>
2024-11-07 13:12:50 +0700

    add folder 'commons'

---
+++ commons/env.php
@@ -0,0 +1,29 @@
+<?php
+// khai bao cac bien moi truong dung global
+define('PATH_CONTROLLER', __DIR__ . '/../controllers/');
+define('PATH_MODEL', __DIR__ . '/../models/');
+define('PATH_VIEW', __DIR__ . '/../views/');
+
+define('PATH_CONTROLLER_ADMIN', __DIR__ . '/../admin/controllers/');
+define('PATH_MODEL_ADMIN', __DIR__ . '/../admin/models/');
+define('PATH_VIEW_ADMIN', __DIR__ . '/../admin/views/');
+
+define('PATH_UPLOAD', __DIR__ . '/../');
+
+define('BASE_URL', 'http://localhost/mvc-basic/');
+
+define('BASE_URL_ADMIN', 'http://localhost/mvc-basic/admin/');
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_NAME', 'sampleProject');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+
+define('STATUS_IN_STOCK', 'in stock');
+define('STATUS_OUT_OF_STOCK', 'out of stock');
+
+define('IS_TRENDING_YES', 'yes');
+define('IS_TRENDING_NO', 'no');
+
+
