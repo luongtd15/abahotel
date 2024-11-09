4579a84855648bbe8498e24224808a1c46051e95
luongtd15 <luongnek121@gmail.com>
2024-11-09 13:02:05 +0700

    dsad

--- commons/env.php
+++ commons/env.php
@@ -10,20 +10,17 @@ define('PATH_VIEW_ADMIN', __DIR__ . '/../admin/views/');
 
 define('PATH_UPLOAD', __DIR__ . '/../');
 
-define('BASE_URL', 'http://localhost/mvc-basic/');
+define('BASE_URL', 'http://localhost/aba-hotel/');
 
-define('BASE_URL_ADMIN', 'http://localhost/mvc-basic/admin/');
+define('BASE_URL_ADMIN', 'http://localhost/aba-hotel/admin/');
 
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
-define('DB_NAME', 'sampleProject');
+define('DB_NAME', 'aba');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 
-define('STATUS_IN_STOCK', 'in stock');
-define('STATUS_OUT_OF_STOCK', 'out of stock');
-
-define('IS_TRENDING_YES', 'yes');
-define('IS_TRENDING_NO', 'no');
+define('STATUS_AVAILABLE', 'available');
+define('STATUS_OCCUPIED', 'occupied');
 
 
